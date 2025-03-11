b01441ed499cf04530d1317240b3f4229499105b
PEKPELI kevin <pekpelignimdoukevin@gmail.com>
2025-03-11 04:12:01 +0000

    making front

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,17 +3,20 @@ import { NextResponse } from "next/server";
 export default async function POST(req: Request) {
     try {
         const { message } = await req.json();
-        const gemini_key = process.env.GEMINI_KEY;
+        const gemini_api = process.env.GEMINI_API;
 
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${gemini_key}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "prompt": message,
-            })
-        });
+        const response = await fetch(
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${gemini_api}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "prompt": message,
+                })
+            }
+        );
 
         const data = await response.json();
         return NextResponse.json(data);
